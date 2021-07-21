9053d275ac99b8d8c69b4bde9f5ef5e5014590ee
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2021-07-21 13:49:19 +0100

    1st Commit

---
+++ comp/TestScene.js
@@ -0,0 +1,28 @@
+class TestScene extends Phaser.Scene
+{
+    constructor ()
+    {
+        super({key: 'TestScene'});
+    }
+
+    preload ()
+    {
+        this.load.image('logo', '../assets/logo.png');
+    }
+      
+    create ()
+    {
+        const logo = this.add.image(400, 150, 'logo');
+      
+        this.tweens.add({
+            targets: logo,
+            y: 150+150,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+    }
+}
+
+export default TestScene
