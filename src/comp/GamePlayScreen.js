132a6050cdd8c4cd2e37d5b0b476b6860612bcac
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2021-08-06 11:43:35 +0100

    !!

--- src/comp/GamePlayScreen.js
+++ src/comp/GamePlayScreen.js
@@ -8,13 +8,13 @@ class GamePlayScreen extends Phaser.Scene
 //=========================================================================================================
 //-----------------------------------------PRELOAD------------------------------------------------------
     preload (){
-        this.load.image('tileSet', '/src/assets/yellow64x64.png')
-        this.load.image('springBack1', '/src/assets/bg_spring_Trees_1.png')
-        this.load.image('springBack2', '/src/assets/bg_spring_Trees_2.png')
-        this.load.tilemapTiledJSON('FirstMap', '/src/assets/mapFirst.json')
-        this.load.atlas('bCoin', '/src/assets/bronze_coin.png', '/src/assets/bronze_coin_atlas.json')
-        this.load.atlas('cherry', '/src/assets/cherry.png', '/src/assets/cherry_atlas.json')
-        this.load.atlas('water', '/src/assets/water.png', '/src/assets/water_atlas.json')
+        this.load.image('tileSet', './src/assets/yellow64x64.png')
+        this.load.image('springBack1', './src/assets/bg_spring_Trees_1.png')
+        this.load.image('springBack2', './src/assets/bg_spring_Trees_2.png')
+        this.load.tilemapTiledJSON('FirstMap', './src/assets/mapFirst.json')
+        this.load.atlas('bCoin', './src/assets/bronze_coin.png', './src/assets/bronze_coin_atlas.json')
+        this.load.atlas('cherry', './src/assets/cherry.png', './src/assets/cherry_atlas.json')
+        this.load.atlas('water', './src/assets/water.png', './src/assets/water_atlas.json')
     }
     //===================================================================================================
     //------------------------------------CREATE-------------------------------------------------------  
